bee16a271014e62d409e64d8550fca84974a64cd
TimWolfskin <timwolfskin013@gmail.com>
2023-02-17 22:10:21 +0300

    changed product

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import { store } from '../redux/store'
-import { Toaster } from 'react-hot-toast'
-import { SessionProvider } from 'next-auth/react'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: {session, ...pageProps}  }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
+    // Higher order component
     <SessionProvider session={session}>
-      <Provider store={store} >
+      <Provider store={store}>
         <Toaster />
         <Component {...pageProps} />
       </Provider>
     </SessionProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
